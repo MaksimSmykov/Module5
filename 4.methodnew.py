04d539d3e4353598dd78f9066e528b5ac4704f49
MaksimSmykov <smykovmaksim23@yandex.ru>
2024-10-05 09:53:47 +0300

    Add files via upload

---
+++ 4.methodnew.py
@@ -0,0 +1,112 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        return print(f"{self.name} снесён, но он останется в истории")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House) and isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, House) and isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+                return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other, House) and isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other, House) and isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other, House) and isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other, House) and isinstance(self.number_of_floors, int) and isinstance(other.number_of_floors, int):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value): #Обычное сложение целых чисел
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __sub__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors = self.number_of_floors - value
+        return self
+
+    def __rsub__(self, value):
+        return self.__sub__(value)
+
+    def __isub__(self, value):
+        return self.__sub__(value)
+
+    def __mul__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors = self.number_of_floors * value
+        return self
+
+    def __rmul__(self, value):
+        return self.__mul__(value)
+
+    def __imul__(self, value):
+        return self.__mul__(value)
+
+    def __truediv__(self, value):
+        if isinstance(self.number_of_floors, int) and isinstance(value, int):
+            self.number_of_floors = int(self.number_of_floors / value)
+        return self
+
+    def __rtruediv__(self, value):
+        return self.__truediv__(value)
+
+    def __itruediv__(self, value):
+        return self.__truediv__(value)
+
+    def go_to(self, new_floor):
+        if (new_floor > 0 and self.number_of_floors > 0 and
+                isinstance(new_floor, int) and isinstance(self.number_of_floors, int)):
+            if new_floor > self.number_of_floors or new_floor < 1:
+                print('Такого этажа не существует')
+            else:
+                for i in range(1, new_floor + 1):
+                    print(i)
+        else:
+            print('Такого этажа не существует')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
